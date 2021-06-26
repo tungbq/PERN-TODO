be3ac17955dc29d7c611c4b1a449d714e7c1b7de
Tung Bui <tungbq.info@gmail.com>
2021-06-26 15:01:46 +0700

    [client] Use window location

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -15,7 +15,7 @@ const InputTodo = () => {
         body: JSON.stringify(body)
       })
 
-      console.log(response)
+      window.location = "/"
     } catch (error) {
       console.error(error.message)
     }
